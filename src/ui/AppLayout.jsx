63122c754b7604f55b16d2c2048dc91fe438c240
Andreas Kowalenko <kowalenko@eddi24.de>
2023-10-11 17:18:33 +0200

    styling part1

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
-    return (
-        <div>
-            <Header />
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
-            <main>
-                <Outlet />
-            </main>
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
-            <CartOverview />
-        </div>
-    );
+      <Header />
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
